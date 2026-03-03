3ce304602e1cacf8c539ce9b4a727c3f812c635d
dougyster <dyang@college.harvard.edu>
2026-03-02 20:31:57 -0800

    init refactor: adding registers, partitions, and refactoring the pr-test workflow

--- tests/e2e/ckpt/test_qwen3_4B_ckpt.py
+++ tests/e2e/ckpt/test_qwen3_4B_ckpt.py
@@ -1,8 +1,9 @@
 import os
-from argparse import ArgumentParser
 
 import miles.utils.external_utils.command_utils as U
+from tests.ci.ci_register import register_cuda_ci
 
+register_cuda_ci(est_time=1200, suite="stage-c-ckpt-8-gpu", num_gpus=8)
 
 ENABLE_EVAL = bool(int(os.environ.get("MILES_TEST_ENABLE_EVAL", "1")))
 TIGHT_HOST_MEMORY = bool(int(os.environ.get("MILES_TEST_TIGHT_HOST_MEMORY", "1")))
@@ -10,10 +11,6 @@ TIGHT_HOST_MEMORY = bool(int(os.environ.get("MILES_TEST_TIGHT_HOST_MEMORY", "1")
 MODEL_NAME = "Qwen3-4B"
 MODEL_TYPE = "qwen3-4B"
 NUM_GPUS = 8
-
-
-parser = ArgumentParser()
-parser.add_argument("--async-save", action="store_true", help="Whether to test async save/load.")
 
 
 def prepare():
@@ -129,10 +126,10 @@ def execute(mode: str = ""):
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    # TODO also use typer
     prepare()
     for proxy_var in ("http_proxy", "https_proxy", "HTTP_PROXY", "HTTPS_PROXY"):
         os.environ.pop(proxy_var, None)
-    execute("save" if not args.async_save else "async_save")
+    execute("save")
+    execute("load")
+    execute("async_save")
     execute("load")
