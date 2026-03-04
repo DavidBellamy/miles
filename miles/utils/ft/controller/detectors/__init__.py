a41bdbf79aada19f676e73838c0262c9af8acd62
fzyzcjy <ch271828n@outlook.com>
2026-03-04 20:06:14 +0800

    feat: update detectors __init__.py with exports and build_detector_chain()

---
+++ miles/utils/ft/controller/detectors/__init__.py
@@ -0,0 +1,30 @@
+from miles.utils.ft.controller.detectors.base import BaseFaultDetector
+from miles.utils.ft.controller.detectors.hang import HangDetector
+from miles.utils.ft.controller.detectors.hardware import HighConfidenceHardwareDetector
+from miles.utils.ft.controller.detectors.mfu_decline import MfuDeclineDetector
+from miles.utils.ft.controller.detectors.nan_loss import NanLossDetector
+from miles.utils.ft.controller.detectors.network import NetworkAlertDetector
+from miles.utils.ft.controller.detectors.training_crash import TrainingCrashDetector
+
+__all__ = [
+    "BaseFaultDetector",
+    "HangDetector",
+    "HighConfidenceHardwareDetector",
+    "MfuDeclineDetector",
+    "NanLossDetector",
+    "NetworkAlertDetector",
+    "TrainingCrashDetector",
+    "build_detector_chain",
+]
+
+
+def build_detector_chain() -> list[BaseFaultDetector]:
+    """Build the default detector chain in priority order (highest first)."""
+    return [
+        HighConfidenceHardwareDetector(),
+        NetworkAlertDetector(),
+        TrainingCrashDetector(),
+        HangDetector(),
+        NanLossDetector(),
+        MfuDeclineDetector(),
+    ]
