8205389212110ba1db69957f5da05b88fb2aab29
fzyzcjy <ch271828n@outlook.com>
2026-03-06 18:22:04 +0800

    WebhookNotifier: explicitly inherit NotificationProtocol

--- miles/utils/ft/platform/notifiers/webhook_notifier.py
+++ miles/utils/ft/platform/notifiers/webhook_notifier.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import httpx
 
+from miles.utils.ft.protocols.platform import NotificationProtocol
 from miles.utils.ft.utils.retry import retry_async_or_raise
 
 logger = logging.getLogger(__name__)
@@ -12,7 +13,7 @@ _MAX_RETRIES = 3
 _INITIAL_BACKOFF_SECONDS = 1.0
 
 
-class WebhookNotifier(abc.ABC):
+class WebhookNotifier(NotificationProtocol, abc.ABC):
     """Base class for webhook-based notifiers with retry and exponential backoff."""
 
     def __init__(self, webhook_url: str) -> None:
