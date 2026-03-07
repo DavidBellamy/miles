68eb16f97f838c1a1f7ddd76231c8312c7a5c1a0
fzyzcjy <ch271828n@outlook.com>
2026-03-07 20:23:15 +0800

    fix: re-export StackTraceAggregator from stack_trace package __init__

--- miles/utils/ft/controller/diagnostics/stack_trace/__init__.py
+++ miles/utils/ft/controller/diagnostics/stack_trace/__init__.py
@@ -1,5 +1,8 @@
+from miles.utils.ft.controller.diagnostics.stack_trace.aggregator import (
+    StackTraceAggregator,
+)
 from miles.utils.ft.controller.diagnostics.stack_trace.collector import (
     collect_stack_trace_suspects,
 )
 
-__all__ = ["collect_stack_trace_suspects"]
+__all__ = ["StackTraceAggregator", "collect_stack_trace_suspects"]
