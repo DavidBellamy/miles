e945aedbb245aa5d207fc3c233d860bc2438fd8b
fzyzcjy <ch271828n@outlook.com>
2026-03-31 10:45:38 +0800

    more

--- miles/ray/train/group.py
+++ miles/ray/train/group.py
@@ -179,23 +179,7 @@ class RayTrainGroup:
         for cell in pending_cells:
             cell.allocate_for_pending()
 
-        # Step 2: All previously-running cells reconfigure indep_dp PG
-        refs: list[ray.ObjectRef] = []
-        for cell in running_cells:
-            refs.extend(cell.async_execute("reconfigure_indep_dp", indep_dp_quorum_id=self._indep_dp_quorum_id))
-        await asyncio.gather(*refs)
-        del refs
-
-        # Step 3: For each pending cell, send ckpt from a running cell + init with recv
-        refs: list[ray.ObjectRef] = []
-        for cell in pending_cells:
-            src_cell = running_cells[0]  # TODO support load balancing
-            refs.extend(src_cell.async_execute("send_ckpt", dst_rank=cell.cell_id))
-            refs.extend(cell.async_execute("init", **self._cell_init_kwargs(), recv_ckpt_src_rank=src_cell.cell_id))
-        await asyncio.gather(*refs)
-        del refs
-
-        logger.info("All pending cells materialized successfully")
+        TODO
 
 
 PGTuple = tuple[PlacementGroup, list[int], list[int]]
