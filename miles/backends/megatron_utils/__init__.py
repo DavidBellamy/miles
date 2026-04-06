9ed410c41610fd8ab7d139d6e2521563b5b9576c
fzyzcjy <ch271828n@outlook.com>
2026-04-06 12:18:19 +0800

    revert

--- miles/backends/megatron_utils/__init__.py
+++ miles/backends/megatron_utils/__init__.py
@@ -9,12 +9,12 @@ try:
     old_init = deep_ep.Buffer.__init__
 
     def new_init(self, *args, **kwargs):
-        if (x := torch_memory_saver._impl) is not None:
-            x._binary_wrapper.cdll.tms_set_interesting_region(False)
+        if torch_memory_saver._impl is not None:
+            torch_memory_saver._impl._binary_wrapper.cdll.tms_set_interesting_region(False)
         old_init(self, *args, **kwargs)
         torch.cuda.synchronize()
-        if (x := torch_memory_saver._impl) is not None:
-            x._binary_wrapper.cdll.tms_set_interesting_region(True)
+        if torch_memory_saver._impl is not None:
+            torch_memory_saver._impl._binary_wrapper.cdll.tms_set_interesting_region(True)
 
     deep_ep.Buffer.__init__ = new_init
 except ImportError:
