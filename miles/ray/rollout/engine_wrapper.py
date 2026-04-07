35b85470ac822c6bee34807ddc43c3951a9b073c
fzyzcjy <ch271828n@outlook.com>
2026-04-07 17:27:34 +0800

    more

--- miles/ray/rollout/engine_wrapper.py
+++ miles/ray/rollout/engine_wrapper.py
@@ -2,6 +2,8 @@ import ray
 from pydantic import BaseModel, ConfigDict
 
 
+# NOTE: better name may be `ServerEngine`, but we explicitly name it `wrapper` here to
+#       avoid confusion w/ the old `engine`s which are indeed ray `ActorHandle`s
 class EngineWrapper:
     def __init__(self):
         self._state = _StateStopped()
